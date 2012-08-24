b00a4dc1d01dd00665e52b8308f7644096be2c38
Ludwig Nussel <ludwig.nussel@suse.de>
2012-08-24 14:08:06 +0200

    keep label explicitly as well as suggested by darix

--- render.py
+++ render.py
@@ -21,7 +21,7 @@ import atexit
 VERSION = "12.2"
 RELEASE = datetime.datetime(2012, 9, 5, 14, 0, 0)
 
-VARIANTS = ["nolabel"]
+VARIANTS = ["label", "nolabel"]
 
 ###--- no need to change below this line ---###
 
