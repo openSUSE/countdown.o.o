092c580f60f6065a80d51218bf7421f0bb53f497
Ludwig Nussel <ludwig.nussel@suse.de>
2013-09-23 11:15:38 +0200

    copy 12.3 files to 13.1 and activate

--- render.py
+++ render.py
@@ -18,9 +18,9 @@ import tempfile
 import shutil
 import atexit
 
-VERSION = "12.3"
+VERSION = "13.1"
 # UTC timestamp!
-RELEASE = datetime.datetime(2013, 3, 13, 14, 0, 0)
+RELEASE = datetime.datetime(2013, 11, 19, 12, 0, 0)
 
 VARIANTS = ["label", "nolabel"]
 
